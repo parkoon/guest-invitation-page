58333eecde3c85dbf7fd3cc54820365f80952e67
jh.park <jh.park@ent-bc.com>
2024-05-07 13:44:15 +0900

    위 경도 수정

--- src/components/map/constants.ts
+++ src/components/map/constants.ts
@@ -1,4 +1,4 @@
-export const lat = 37.54997889999928;
-export const lng = 127.11763375393691;
-export const destination = encodeURIComponent("광나루한강공원 테니스장");
+export const lat = 37.549923687964984;
+export const lng = 127.1217881563696;
+export const destination = encodeURIComponent("광나루한강공원테니스장");
 export const address = "서울특별시 강동구 선사로 83-106";
